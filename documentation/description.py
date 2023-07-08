c7c1eae3567127f731d5f82e1a7c03d706e2cf44
KhadijaCap4 <khadija.sy@cap4lab.com>
2023-07-08 20:24:31 +0200

    Update description.py

--- documentation/description.py
+++ documentation/description.py
@@ -1,6 +1,7 @@
 # Description
 api_description = description = """ 
-Flowers API helps you do awesome stuff. 
+
+Welcome to the Flower Bouquet API documentation. This API allows you to browse and purchase flower bouquets available in our store. The API provides endpoints to retrieve information about bouquets, manage customer accounts, and process transactions.
 
 ## Bouquets
 
