2f8e293b00a3231186e17a6cbea8c13a60e8a030
KhadijaCap4 <khadija.sy@cap4lab.com>
2023-07-08 20:29:23 +0200

    s

--- documentation/tags.py
+++ documentation/tags.py
@@ -1,16 +1,12 @@
 # Liste des tags utilises dans la doc
 tags_metadata = [
     {
-        "name" : "Flowers",
-        "description" : " Manage flowers. So _Fancy_ they have their own docs.",
+        "name": "Bouquets",
+        "description": "Create and list bouquets of our API",
         "externalDocs" : {
             "description" :"Items external docs",
             "url": "https://fastapi.tiangolo.com/",
         },
-    },
-    {
-        "name": "Bouquets",
-        "description": "Create and list bouquets of our API",
     },
     {
         "name": "Customers",
